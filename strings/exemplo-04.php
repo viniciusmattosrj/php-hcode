4ab88606e54b932f99a3e057457515cb99cb3eb5
Vinicius Mattos <vinimattos.rj@gmail.com>
2018-01-09 15:41:30 -0200

    Strings em PHP - Parte III

--- strings/exemplo-04.php
+++ strings/exemplo-04.php
@@ -2,6 +2,14 @@
 
     /* MANIPULANDO STRINGS */
     $frase = "A repetição é mãe da retenção";
-    $q = strpos($frase, "mãe");
 
-    var_dump($q);
+    $palavra = "mãe";
+
+    $q = strpos($frase, $palavra);
+
+    $texto = substr($frase,0,$q);
+    var_dump($texto);
+
+    $texto2 = substr($frase, $q + strlen($palavra), strlen($frase));
+    echo "<br/>";
+    var_dump($texto2);
