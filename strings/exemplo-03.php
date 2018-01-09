a2394eeaedb0ef861bc80a54d4f6e7692fc45b35
Vinicius Mattos <vinimattos.rj@gmail.com>
2018-01-09 15:34:23 -0200

    Strings em PHP - Parte III

---
+++ strings/exemplo-03.php
@@ -0,0 +1,14 @@
+<?php
+
+    /* MANIPULANDO STRINGS */
+    $empresa = "Hcode";
+
+    $empresa = str_replace("o","0",$empresa);
+    echo $empresa ."<br/>";
+
+    $empresa = str_replace("e","3",$empresa);
+    echo $empresa ."<br/>";
+
+
+
+
---
+++ strings/exemplo-04.php
@@ -0,0 +1,7 @@
+<?php
+
+    /* MANIPULANDO STRINGS */
+    $frase = "A repetição é mãe da retenção";
+    $q = strpos($frase, "mãe");
+
+    var_dump($q);
