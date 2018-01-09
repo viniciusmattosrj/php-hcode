b2929b3fd338fd230f066e2cf231bb697ca88aa3
Vinicius Mattos <vinimattos.rj@gmail.com>
2018-01-09 17:36:33 -0200

    Estruturas de Controle e Laços de repetição - Parte III

---
+++ for/exemplo-01.php
@@ -0,0 +1,11 @@
+<?php
+    
+    /* CONTROLE DE FLUXO - FOR */
+    for($i = 0; $i < 1000; $i+=5)
+    {
+        if($i >= 200 && $i <= 800) continue;
+        if($i === 900) break;
+        {
+            echo $i."<br/>";
+        }        
+    }
