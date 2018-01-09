262de0f03109d7fb0f2a179974d6dfb5af257ba8
Vinicius Mattos <vinimattos.rj@gmail.com>
2018-01-09 15:26:14 -0200

    Strings em PHP - Parte II

--- strings/exemplo-02.php
+++ strings/exemplo-02.php
@@ -1,5 +1,20 @@
 <?php
 
+    /* MANIPULANDO STRINGS */
+    $nome = "joão rangel";
+    echo $nome ."<br/>";
+
+    // Convertendo em Maiusculo e Minusculo 
+    echo strtoupper($nome) ."<br/>";
+    echo strtolower($nome) ."<br/>";
+
+    // Convertendo em Primeira em Maiusculo 
+    echo ucfirst($nome) ."<br/>";
+    echo ucwords($nome) ."<br/>";
+
+
+
+
 
     
  
