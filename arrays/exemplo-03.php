cf59786471e950051f5d60195b2ad4187ffbfceb
Vinicius Mattos <vinimattos.rj@gmail.com>
2018-01-09 18:49:36 -0200

    Arrays em PHP - Parte III

--- arrays/exemplo-03.php
+++ arrays/exemplo-03.php
@@ -1,3 +1,18 @@
 <?php
     
     /* ARRAYS */
+    $pessoas = array();
+
+    array_push($pessoas, array(
+        'nome'=>'Vinícius',
+        'idade'=>30
+    ));
+
+    array_push($pessoas, array(
+        'nome'=>'Fernanda',
+        'idade'=>29 
+    ));
+
+    echo "<pre>";
+    print_r($pessoas[0]['nome']);
+    echo "</pre>";
