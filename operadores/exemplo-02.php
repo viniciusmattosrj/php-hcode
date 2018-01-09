82cdb8d2d549e6a70f7ec95437fc29e07e8580cd
Vinicius Mattos <vinimattos.rj@gmail.com>
2018-01-09 12:03:51 -0200

    Conhecendo os Operadores do PHP - Parte II

---
+++ operadores/exemplo-02.php
@@ -0,0 +1,10 @@
+<?php 
+
+    /* CLASSE OPERADORES ATRIBUIÇÃO */
+    $valorTotal = 0;
+    $valorTotal += 100;
+    $valorTotal += 25;
+
+    $valorTotal *= .9;
+    
+    echo $valorTotal;
---
+++ operadores/exemplo-03.php
@@ -0,0 +1,20 @@
+<?php 
+
+    /* CLASSE OPERADORES ARITIMETICOS */
+    $a = 10;
+    $b = 12;
+
+    echo $a + $b;
+    echo "<br/>";
+
+    echo $a - $b;
+    echo "<br/>";
+
+    echo $a * $b;
+    echo "<br/>";
+
+    echo $a / $b;
+    echo "<br/>";
+    
+    echo $a % $b;
+    echo "<br/>";
