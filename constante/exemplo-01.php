71239da2b145cfdf2197b66810f7389ec4bcfb67
Vinicius Mattos <vinimattos.rj@gmail.com>
2018-01-09 19:33:18 -0200

    Arrays em PHP - Parte VI

---
+++ constante/exemplo-01.php
@@ -0,0 +1,5 @@
+<?php
+    
+    /* CONSTANTE */
+    define("SERVIDOR","127.0.0.1");
+    echo SERVIDOR;
---
+++ constante/exemplo-02.php
@@ -0,0 +1,11 @@
+<?php
+    
+    /* CONSTANTE - ARRAY CONSTANTE */
+    define("BANCO_DE_DADOS",[
+        '127.0.0.1',
+        'root',  
+        'password', 
+        'teste' 
+    ]);
+
+    print_r(BANCO_DE_DADOS);
