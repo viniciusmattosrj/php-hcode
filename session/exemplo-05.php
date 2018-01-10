994fec57f96a7a389092eedce57424021700071c
Vinicius Mattos <vinimattos.rj@gmail.com>
2018-01-10 11:02:34 -0200

    Sessão no PHP 7 - Parte III

--- session/exemplo-05.php
+++ session/exemplo-05.php
@@ -1,5 +1,24 @@
 <?php
 
+    /* SESSION LOCAL ARMAZENAMENTO */
     require_once("config.php");
 
     echo session_save_path();
+    echo "<br>";
+
+    var_dump(session_status());
+    switch(session_status())
+    {
+        case 0:
+            echo " - Sessões desabilitadas";
+            break;
+        case 1:
+            echo " - Sessões habilitadas, mas nenhuma existe.";
+            break;
+        case 2:
+            echo " - Sessões habilitadas, existe uma ativa.";
+            break;
+        default:
+            echo " - Opção Inválida";
+            break;
+    }
