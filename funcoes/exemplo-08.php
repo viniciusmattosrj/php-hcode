88f94553ac8087fb422bffb8f5131a0049107d7f
Vinicius Mattos <vinimattos.rj@gmail.com>
2018-01-10 12:29:41 -0200

    Passagem de parametro Valor e Referencia - Parte IV

---
+++ funcoes/exemplo-08.php
@@ -0,0 +1,13 @@
+<?php
+    /* DECLARAÇÃO TIPOS ESCALARES - Novidades PHP 7 */
+    function soma(float ...$valores):float
+    {
+        return array_sum($valores);
+    }
+
+    echo var_dump(soma(2,2));
+    echo "<br>";
+    echo soma(25,33);
+    echo "<br>";
+    echo soma(1.5,3.2);
+    echo "<br>";
