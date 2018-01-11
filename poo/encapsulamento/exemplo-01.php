30f2838b1b1fca89f48999cb76d299fc9312af5f
Vinicius Mattos <vinimattos.rj@gmail.com>
2018-01-11 00:07:55 -0200

    Métodos Mágicos - Encapsulamento

---
+++ poo/encapsulamento/exemplo-01.php
@@ -0,0 +1,31 @@
+<?php
+
+    /* POO - MODIFICADORES DE ACESSO - ENCAPSULAMENTO */    
+    class Pessoa{
+        public $nome = "Ramus Lerdorf";
+        protected $idade = 48;
+        private $senha = "123456";
+
+        public function verDados(){
+            echo $this->nome . "<br>";
+            echo $this->idade . "<br>";
+            echo $this->senha . "<br>";
+        }
+    }
+
+    $objeto = new Pessoa();
+    //echo $objeto->senha ."<br>";
+    $objeto->verDados();
+                
+
+
+
+
+            
+            
+            
+            
+            
+            
+            
+ 
