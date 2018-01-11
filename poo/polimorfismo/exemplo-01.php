c74c50e1f584ddcaf82c6b7cb4f87af96643d36b
Vinicius Mattos <vinimattos.rj@gmail.com>
2018-01-11 12:21:11 -0200

    POO - Polimorfismo

---
+++ poo/polimorfismo/exemplo-01.php
@@ -0,0 +1,67 @@
+<?php
+    require_once "../../exibe-erros.php";
+
+    /* POO - POLIMORFISMO */
+    abstract class Animal
+    {
+        public function falar()
+        {
+            return "Som";
+        }
+
+        public function mover()
+        {
+            return "Anda";
+        }
+    }
+
+    class Cachorro extends Animal
+    {
+        public function falar()
+        {
+            return "Late";
+        }
+    }
+
+    class Gato extends Animal
+    {
+        public function falar()
+        {
+            return "Mia";
+        }
+    }
+
+    class Passaro extends Animal
+    {
+        public function falar()
+        {
+            return "Canta";
+        }
+
+        public function mover()
+        {
+            return "Voa e " . parent::mover();
+        }
+    }
+
+    /* INSTANCIANDO OBJETOS */
+    $pluto = new Cachorro();
+    echo $pluto->falar() . "<br>";
+    echo $pluto->mover() . "<br>";
+
+    echo "- - - - - - - - - <br>";
+
+    $garfield = new Gato();
+    echo $garfield->falar() . "<br>";
+    echo $garfield->mover() . "<br>";
+
+    echo "- - - - - - - - - <br>";
+
+    $ave = new Passaro();
+    echo $ave->falar() . "<br>";
+    echo $ave->mover() . "<br>";
+
+    
+     
+
+    
