33a313ae25c3663a1c6797353af87344d7b80fdd
Vinicius Mattos <vinimattos.rj@gmail.com>
2018-01-11 17:55:44 -0200

    Namespace

---
+++ namespace/index.php
@@ -0,0 +1,13 @@
+<?php
+	
+	require_once("config.php");
+
+	use Cliente\Cadastro;
+
+	$cad = new Cadastro();
+
+	$cad->setNome("Djalma Sindeaux");
+	$cad->setEmail("djalma@hcode.com.br");
+	$cad->setSenha("123456");
+
+	$cad->registrarVenda();
