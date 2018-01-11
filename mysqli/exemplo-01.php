5f29ef3240e0f54826e6f8abbfd1af959396c678
Vinicius Mattos <vinimattos.rj@gmail.com>
2018-01-11 20:01:15 -0200

    DataBase MySQLi - Parte II

--- mysqli/exemplo-01.php
+++ mysqli/exemplo-01.php
@@ -1,9 +1,25 @@
 <?php
 
-    /* DATABASE - Classe MySQLi */
-    $conn = new mysqli("localhost","root","","dbphp7");
+    $conn = new mysqli("localhost","root","#Dev@8481","dbphp7");
 
-    if($con->connect_error)
+    if($conn->connect_error)
     {
-        echo "Error: " .$conn->connect_error;
+        echo "Error: " . $conn->connect_error;
     }
+
+    $stmt = $conn->prepare("INSERT INTO tb_usuarios (deslogin, dessenha) VALUES(?, ?)");
+
+    $stmt->bind_param("ss",$login,$pass);
+
+    $login = "user";
+    $pass = "12345";
+
+    $stmt->execute();
+
+    $login = "vinicius";
+    $pass = "Mudar123";
+
+    $login = "mattos";
+    $pass = "123Mudar";
+
+    $stmt->execute();
---
+++ mysqli/exemplo-02.php
@@ -0,0 +1,17 @@
+<?php
+
+    $conn = new mysqli("localhost","root","","dbphp7");
+
+    if($conn->connect_error)
+    {
+        echo "Error: " . $conn->connect_error;
+    }
+
+    $result = $conn->query("SELECT * FROM tb_usuarios ORDER BY deslogin");
+
+    while($row = $result->fetch_array(MYSQLI_ASSOC))
+    {
+        echo "<pre>";
+        var_dump($row);
+        echo "</pre>";
+    }
