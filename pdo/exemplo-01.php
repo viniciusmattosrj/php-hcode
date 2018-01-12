c048e772496cdeb2432f734e27660e942b84f7ad
Vinicius Mattos <vinimattos.rj@gmail.com>
2018-01-12 16:41:18 -0200

    PDO: Select

--- pdo/exemplo-01.php
+++ pdo/exemplo-01.php
@@ -1,6 +1,6 @@
 <?php
 
-	/* DATABASE - PDO INSERT */
+	/* DATABASE - PDO SELECT */
 	$conn = new PDO("mysql:dbname=dbphp7;host=localhost","root","");
 
 	$stmt = $conn->prepare("SELECT * FROM tb_usuarios ORDER BY deslogin");
