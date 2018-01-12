4562157611202411351b84e9d8bba70b084635d4
Vinicius Mattos <vinimattos.rj@gmail.com>
2018-01-12 16:34:09 -0200

    PDO: Update dados no banco

---
+++ pdo/exemplo-04.php
@@ -0,0 +1,19 @@
+<?php
+	
+	$conn = new PDO("mysql:dbname=dbphp7;host=localhost","root","");
+
+	$stmt = $conn->prepare("UPDATE tb_usuarios SET deslogin = :LOGIN, dessenha = :PASSWORD WHERE id_usuario = :ID");
+
+	$id = 4;
+
+	$login = "Vinicius";
+
+	$password = "asdfg";
+
+	$stmt->bindParam(":LOGIN",$login);
+	$stmt->bindParam(":PASSWORD",$password);
+	$stmt->bindParam(":ID",$id);
+
+	$stmt->execute();
+
+	echo "Alterado Ok";
