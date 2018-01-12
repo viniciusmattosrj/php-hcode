cc64f072d1e6629ea6711ca0931a4ade6e34a85e
Vinicius Mattos <vinimattos.rj@gmail.com>
2018-01-12 21:12:41 -0200

    PDO: DAO Select

--- DAO/config/config.php
+++ DAO/config/config.php
@@ -1,9 +1,10 @@
 <?php
-
+	
+	/* PDO - DAO */
 	spl_autoload_register(function($class_name)
 	{
 
-		$filename = $class_name.".php";
+		$filename = "class" . DIRECTORY_SEPARATOR . $class_name.".php";
 
 		if(file_exists(($filename)))
 		{
