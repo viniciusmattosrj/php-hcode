092734eecb37d7e7d31ba09c188a91a7af747465
Vinicius Mattos <vinimattos.rj@gmail.com>
2018-01-12 15:58:22 -0200

    PDO: Inserindo dados no banco

---
+++ pdo/exemplo-03.php
@@ -0,0 +1,19 @@
+<?php
+
+	/* DATABASE - PDO */
+	$conn = new PDO("mysql:dbname=dbphp7;host:localhost","root","");
+
+	/* O statement = comando */
+	$stmt = $conn->prepare("INSERT INTO tb_usuarios(deslogin, dessenha) VALUES(:LOGIN,:PASSWORD)");
+
+	$login = "jose";
+	$senha = "1234567890";
+
+	/* O método bind significar um unir/ligar */
+	$stmt->bindParam(":LOGIN",$login);
+	$stmt->bindParam(":PASSWORD",$senha);
+
+	$stmt->execute();
+
+	echo "Inserido Ok";
+
