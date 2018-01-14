6552a256800fe55e92dec7d2d351a93ae159a5d5
Vinicius Mattos <vinimattos.rj@gmail.com>
2018-01-14 17:46:36 -0200

    Manipulando Arquivos - Movendo um arquivo

---
+++ Rename/exemplo-01.php
@@ -0,0 +1,22 @@
+<?php
+
+	$dir1 = "folder_01";
+	$dir2 = "folder_02";
+
+	/* Por ser uma instrução curta não foi necessário utilizar as chaves e pode ser tudo na mesma linha "CLEAN CODE" ;-) */ 
+	if(!is_dir($dir1)) mkdir($dir1);
+	if(!is_dir($dir2)) mkdir($dir2);
+
+	$filename = "README.txt";
+
+	if(!file_exists($dir1 . DIRECTORY_SEPARATOR . $filename))
+	{
+		$file = fopen($dir1 . DIRECTORY_SEPARATOR . $filename, "w+");
+
+		fwrite($file, date("Y/m/d H:i:s"));
+
+		fclose($file);
+	}
+
+
+
