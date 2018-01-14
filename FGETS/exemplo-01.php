0bdf1b9eab5f6a62b5adb7e94c7d238cb59728b4
Vinicius Mattos <vinimattos.rj@gmail.com>
2018-01-14 16:00:21 -0200

    Manipulando Arquivos -  FGETS

---
+++ FGETS/exemplo-01.php
@@ -0,0 +1,32 @@
+<?php
+
+    /* MANIPULANDO ARQUIVOS - FEGTS */
+    $filename = "usuarios.csv";
+
+    //Verificação para saber se de fato o arquivo existe.
+    if(file_exists($filename))
+    {
+        $file = fopen($filename,"r");
+
+        $headers = explode(",",fgets($file));
+
+        $data = array();
+  
+        while($row = fgets($file))
+        {
+            $rowData = explode(",",$row);
+            $linha = array();
+
+            for($i = 0; $i < count($headers); $i++)
+            {   
+                $linha[$headers[$i]] = $rowData[$i];
+            }
+
+            array_push($data, $linha);
+        }
+
+        fclose($file);
+
+        echo json_encode($data);
+    }
+
