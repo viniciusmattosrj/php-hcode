59a4d30275503d9ca4d16ae8abf782fb70578ce7
Vinicius Mattos <vinimattos.rj@gmail.com>
2018-01-15 18:31:41 -0200

    Biblioteca GD - Escrevendo em Imagens

--- GD/exemplo-02.php
+++ GD/exemplo-02.php
@@ -1,6 +1,6 @@
 <?php
 	
-	/* BIBLIOTECA GD */
+	/* BIBLIOTECA GD - Escrevendo em Imagens */
 	$image = imagecreatefromjpeg("certificado.jpg");
 
 	$titleColor = imagecolorallocate($image, 0, 0, 0);
@@ -8,7 +8,7 @@
 
 	imagestring($image, 5, 450, 150, "CERTIFICADO", $titleColor);
 	imagestring($image, 5, 440, 350, "Marcos Vinicius", $titleColor);
-	imagestring($image, 3, 440, 370, utf8_decode("Concluído em: ").date("d/m/Y"), $titleColor);
+	imagestring($image, 3, 440, 370, "Concluído em: " .date("d/m/Y"), $titleColor);
 
 	header("Content-type: image/jpeg");
 
