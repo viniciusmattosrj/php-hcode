db691df52f40e6f7eec583ed0a9e8d86cc354a19
Vinicius Mattos <vinimattos.rj@gmail.com>
2018-01-15 10:57:34 -0200

    Manipulando Arquivos - Download

--- DOWNLOAD/exemplo-01.php
+++ DOWNLOAD/exemplo-01.php
@@ -1,5 +1,6 @@
 <?php
 
+	/* MANIPULANDO ARQUIVOS - DOWNLOAD */
 	$link = "https://www.google.com.br/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png";
 
 	$content = file_get_contents($link);
