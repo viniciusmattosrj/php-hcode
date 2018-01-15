85c89e5f10c9f69cb55790aec299d60d8a540feb
Vinicius Mattos <vinimattos.rj@gmail.com>
2018-01-15 10:53:09 -0200

    Manipulando Arquivos - Download

---
+++ DOWNLOAD/exemplo-01.php
@@ -0,0 +1,19 @@
+<?php
+
+	$link = "https://www.google.com.br/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png";
+
+	$content = file_get_contents($link);
+
+	/* Realizei uma conversão da imagem */
+	$parse = parse_url($link);
+
+	$basename = basename($parse["path"]);
+
+	$file = fopen($basename, "w+");
+
+	fwrite($file, $content);
+
+	fclose($file);
+?>
+<!-- Quando eu abro o php e logo em seguida acrescento o igual significa que estou fazendo a função echo -->
+<img src="<?=$basename?>">
