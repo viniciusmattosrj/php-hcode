c8ae311bb096b5f048d9ef3d6828ab7d72369500
Vinicius Mattos <vinimattos.rj@gmail.com>
2018-01-15 15:23:52 -0200

    Try e Catch - Parte II

---
+++ erros/exemplo-02.php
@@ -0,0 +1,7 @@
+<?php
+	
+	/* Try e Catch */
+	// O codigo abaixo irá gerar um Notice, que para não ser exibida para usuário deverá ser tratado com if, isset...
+	$nome = $_GET["nome"];
+
+	echo $nome;
