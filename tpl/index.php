2ac1f65fa25c469c345586b9abb520be9f3e8752
Vinicius Mattos <vinimattos.rj@gmail.com>
2018-01-16 14:35:12 -0200

    Templates com RainTPL

---
+++ tpl/index.php
@@ -0,0 +1,22 @@
+<?php
+
+	require_once("vendor/autoload.php");
+
+	use Rain\Tpl;
+
+	$config = array(
+		"tpl_dir"    => "tpl/",
+		"cache_dir"  => "cache/"
+	);
+	Tpl::configure( $config );
+
+	$tpl = new Tpl;
+	
+	/*
+		$tpl->assign( "name", " Obi Wan Kenoby" );
+	*/
+	
+	/*
+		$tpl->assign( "Week", array( "Monday", "Tuesday", "Wednesday", "Thurday", "Friday", "Saturday", "Sunday"));
+	*/
+	$tpl->draw( "index" );
