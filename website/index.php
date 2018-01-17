b30151df8ac460158336028a3c85be4f4389d95f
Vinicius Mattos <vinimattos.rj@gmail.com>
2018-01-17 01:05:54 -0200

    SLIM - Rotas

---
+++ website/index.php
@@ -0,0 +1,20 @@
+<?php
+	
+	/* MICRO FRAMEWORK - SLIM */
+	require_once("vendor/autoload.php");
+
+	$app = new \Slim\Slim();
+
+	$app->get('/', function()
+	{
+		echo json_encode(array(
+			'date'=>date("Y-m-d H-i-s")
+		));
+	});
+
+	$app->get('/hello/:name', function($name)
+	{
+		echo "Hello, " .$name;
+	});
+
+	$app->run();
